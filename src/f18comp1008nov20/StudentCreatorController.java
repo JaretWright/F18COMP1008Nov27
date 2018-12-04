5721153e40f0275231a4e1f45b33497fa3967e07
JaretWright <hkkA223>
2018-12-04 06:13:44 -0500

    Added new functionality for RadioButton

--- src/f18comp1008nov20/StudentCreatorController.java
+++ src/f18comp1008nov20/StudentCreatorController.java
@@ -7,13 +7,17 @@ package f18comp1008nov20;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -42,7 +46,19 @@ public class StudentCreatorController implements Initializable {
     @FXML
     private Label errMsgLabel;
     
+    @FXML
+    private Label ageLabel;
     
+    @FXML
+    private Button button;
+    
+    @FXML
+    private RadioButton trueRadioButton;
+    
+    @FXML
+    private RadioButton falseRadioButton;
+    
+    private ToggleGroup toggleGroup;
     
     /**
      * Initializes the controller class.
@@ -57,7 +73,26 @@ public class StudentCreatorController implements Initializable {
         
         errMsgLabel.setText("");
         
+        toggleGroup = new ToggleGroup();
+        trueRadioButton.setToggleGroup(toggleGroup);
+        falseRadioButton.setToggleGroup(toggleGroup);
+        falseRadioButton.setSelected(true);
+        
+        toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (((RadioButton) newValue).getText().equalsIgnoreCase("true"))
+                button.setDisable(true);
+            else
+                button.setDisable(false);
+        });
+           
     }    
+    
+    @FXML
+    public void radioButtonChanged()
+    {
+        if (toggleGroup.getSelectedToggle() != null)
+            System.out.println(toggleGroup.getSelectedToggle().getUserData().toString());
+    }
     
     @FXML
     public void genderChanger()
@@ -77,6 +112,9 @@ public class StudentCreatorController implements Initializable {
         if (this.birthdayDatePicker.getValue() != null)
         {
             try{
+                
+                System.out.println(((RadioButton) toggleGroup.getSelectedToggle()).getText());
+                
                 Student newStudent = new Student(1, 
                                         this.firstNameTextField.getText(),
                                         this.lastNameTextField.getText(),
@@ -95,4 +133,18 @@ public class StudentCreatorController implements Initializable {
         
     }
     
+    @FXML
+    public void datePickerChanged()
+    {
+        Period period = Period.between(birthdayDatePicker.getValue(), LocalDate.now());
+        int years = period.getYears();
+        int months = period.getMonths();
+        int days = period.getDays();
+        
+        if (years>=1)
+            ageLabel.setText(String.format("%d year%s old",years,(years==1?"":"s")));
+        else 
+            ageLabel.setText(String.format("%d months and %d days", months, days));
+            
+    }
 }
